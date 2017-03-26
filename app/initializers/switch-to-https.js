1f4c4dc032b277757529800d37ccd26b3237a4bf
Seena <seenarowhani95@gmail.com>
2017-03-26 16:08:34 -0400

    only redirect if production

--- app/initializers/switch-to-https.js
+++ app/initializers/switch-to-https.js
@@ -1,4 +1,9 @@
+import config from '../config/environment';
+
 export function initialize () {
+  if (config.environment !== 'production')
+    return
+
   if (window.location.protocol === 'http:') {
     window.location.protocol = 'https:'
   }
